a37d74a39ada65f40fce9b33dc9aa91d483a8110
ZahraEm <zahra.emami7@gmail.com>
2024-03-31 00:57:37 +0330

    feat: add On push strategy for input number

--- src/app/components/custom-input-number/custom-input-number.component.ts
+++ src/app/components/custom-input-number/custom-input-number.component.ts
@@ -1,14 +1,19 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
-import {FormsModule} from "@angular/forms";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-custom-input-number',
   standalone: true,
-  imports: [
-    FormsModule
-  ],
+  imports: [FormsModule],
   templateUrl: './custom-input-number.component.html',
-  styleUrl: './custom-input-number.component.scss'
+  styleUrl: './custom-input-number.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CustomInputNumberComponent {
   @Input() value: number = 0;
@@ -22,14 +27,14 @@ export class CustomInputNumberComponent {
   }
 
   decrement() {
-    if (this.value - this.step >= this.min) { // Check if the next decrement would go below min
+    if (this.value - this.step >= this.min) {
       this.value -= this.step;
       this.emitValueChange();
     }
   }
 
   onChange() {
-    if (this.value < this.min) { // Ensure value is not below min after manual input
+    if (this.value < this.min) {
       this.value = this.min;
     }
     this.emitValueChange();
